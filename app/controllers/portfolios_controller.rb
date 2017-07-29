a91c7153791a0fa516d61945f4b455948e2449ff
Sergio Aravena <sergt26@hotmail.com>
2017-07-29 13:14:26 -0400

    Updated portfolio to incude attr for position and added link to homepage for my name

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,7 +4,7 @@ class PortfoliosController < ApplicationController
   access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   # before_action :portfolio_params
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.by_position
   end
 
   def angular
